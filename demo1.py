33e345792f9e9eb72ecf081f41040023a9dc39dd
YashvanthB2498 <138599983+YashvanthB2498@users.noreply.github.com>
2023-09-14 00:17:14 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,1 +1,8 @@
+# Define a function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Call the function and print the result
+result = add_numbers(5, 3)
+print("The result of adding 5 and 3 is:", result)
 
