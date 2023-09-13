533a4aac675fd6a23dd5209b2703f21ca9440b0a
YashvanthB2498 <138599983+YashvanthB2498@users.noreply.github.com>
2023-09-14 00:17:49 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,1 +1,8 @@
+# Define a function to add two numbers
+def add_numbers(a, b):
+    return a - b
+
+# Call the function and print the result
+result = sub_numbers(5, 3)
+print("The result of subtract 5 and 3 is:", result)
 
